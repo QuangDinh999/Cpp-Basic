6044d1204db839d6b6f759bdeaec2cae514cfd7a
QuangDinh999 <92561923+QuangDinh999@users.noreply.github.com>
2021-10-26 13:57:44 +0700

    bt05.cpp

---
+++ 05/05.cpp
@@ -0,0 +1,112 @@
+// 05.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	using namespace std;
+	/*short number;
+	string name;
+
+	cout << "1.nhap so hai chu so: ";
+	cin >> number;
+	cin.ignore(100, '\n');
+	cout << "1.viet bang tieng viet: ";
+	cin >> name;
+	getline(cin, name);
+	cout << "2.nhap so hai chu so: ";
+	cin >> number;
+	cin.ignore(100, '\n');
+	cout << "2.viet bang tieng viet: ";
+	cin >> name;
+	getline(cin, name);
+	cout << "3.nhap so hai chu so: ";
+	cin >> number;
+
+	cin.ignore(100, '\n');
+
+	cout << "3.viet bang tieng viet: ";
+	cin >> name;
+	getline(cin, name);*/
+
+
+	/*int a;
+	int b;
+	double nghiem;
+	cout << "a = ";
+	cin >> a;
+	cout << "b = ";
+	cin >> b;
+	if (a == 0) {
+		if (b == 0) {
+			cout << "phuong trinh vo so nghiem " << '\n';
+		}
+		else {
+			cout << "phuong trinh vo nghiem " << '\n';
+		}
+	}
+	else {
+		nghiem = (double)-b / a;
+		cout << "phuong trinh bac 1 co nghiem la:" << nghiem << '\n';
+	}*/
+	
+	
+	/*int a;
+	int b;
+	double nghiem;
+	cout << "Nhap a = ";
+	cin >> a;
+	cout << "Nhap b = ";
+	cin >> b;
+	if (a == 0) {
+		if (b == 0) {
+			cout << "Phuong trinh vo so nghiem" << '\n';
+		}
+		else {
+			cout << "Phuong trinh vo nghiem" << '\n';
+		}
+	}
+	else {
+		nghiem = (double)-b / a;
+		cout << "Phuong trinh co nghiem la: " << nghiem << '\n';
+	}*/
+
+	int a;
+	int b;
+	
+	/*cout << " input two intergers print to console max,min ";*/
+	
+	cout << "Enter two number randomly: ";
+	cin >> a >> b;
+	
+
+	if (a < b) {
+		cout << a << " is the min number" << '\n';
+		cout << b << " is the max number" << '\n';
+	}
+	else if (a > b) {
+		cout << a << "is the max number" << '\n';
+		cout << b << "is the min number" << '\n';
+	}
+	else {
+		cout << "error" << '\n';
+	}
+	
+
+
+
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
